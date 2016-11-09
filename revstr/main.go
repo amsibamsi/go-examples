5e5c2ad59450a31032d86ccea73fbb50236f49ff
Anselm Strauss <strauss@puzzle.ch>
2016-11-09 14:06:08 +0100

    reverse string: variants and benchmarks

--- revstr/main.go
+++ revstr/main.go
@@ -2,11 +2,38 @@ package revstr
 
 import "bytes"
 
-func ReverseString(s string) string {
-	chars := []rune(s)
-	var buf bytes.Buffer
-	for i := len(chars) - 1; i >= 0; i-- {
-		buf.WriteRune(chars[i])
+func ReverseStringBuffer(s string) string {
+	r := []rune(s)
+	var b bytes.Buffer
+	for i := len(r) - 1; i >= 0; i-- {
+		b.WriteRune(r[i])
 	}
-	return buf.String()
+	return b.String()
+}
+
+func ReverseStringAppend(s string) string {
+	r := []rune(s)
+	t := []rune{}
+	for i := len(r) - 1; i >= 0; i-- {
+		t = append(t, r[i])
+	}
+	return string(t)
+}
+
+func ReverseStringIndex(s string) string {
+	r := []rune(s)
+	var t []rune
+	t = make([]rune, len(r))
+	for i := 0; i < len(r); i++ {
+		t[i] = r[len(r)-1-i]
+	}
+	return string(t)
+}
+
+func ReverseStringInplace(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
--- revstr/main_test.go
+++ revstr/main_test.go
@@ -2,21 +2,66 @@ package revstr
 
 import "testing"
 
-func TestReverseString(t *testing.T) {
-	tests := []struct {
+var (
+	Tests = []struct {
 		original, reversed string
 	}{
 		{"", ""},
 		{"x", "x"},
 		{"ab", "ba"},
 		{"my string", "gnirts ym"},
+		{"ê獅", "獅ê"},
 	}
-	for _, test := range tests {
+	Benchmark = "abcdefghijklmnopqrtsuvwxyz1234567890"
+)
+
+type Reverser func(string) string
+
+func test(r Reverser, t *testing.T) {
+	for _, test := range Tests {
 		t.Run(test.original, func(t *testing.T) {
-			r := ReverseString(test.original)
-			if r != test.reversed {
-				t.Errorf("got '%s', expected '%s'", r, test.reversed)
+			s := r(test.original)
+			if s != test.reversed {
+				t.Errorf("got '%s', expected '%s'", s, test.reversed)
 			}
 		})
 	}
+}
+
+func TestReverseStringBuffer(t *testing.T) {
+	test(ReverseStringBuffer, t)
+}
+
+func TestReverseStringAppend(t *testing.T) {
+	test(ReverseStringAppend, t)
+}
+
+func TestReverseStringIndex(t *testing.T) {
+	test(ReverseStringIndex, t)
+}
+
+func TestReverseStringInplace(t *testing.T) {
+	test(ReverseStringInplace, t)
+}
+
+func benchmark(r Reverser, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		r(Benchmark)
+	}
+}
+
+func BenchmarkReverseStringBuffer(b *testing.B) {
+	benchmark(ReverseStringBuffer, b)
+}
+
+func BenchmarkReverseStringAppend(b *testing.B) {
+	benchmark(ReverseStringAppend, b)
+}
+
+func BenchmarkReverseStringIndex(b *testing.B) {
+	benchmark(ReverseStringIndex, b)
+}
+
+func BenchmarkReverseStringInplace(b *testing.B) {
+	benchmark(ReverseStringInplace, b)
 }
