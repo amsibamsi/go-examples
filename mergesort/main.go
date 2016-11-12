5ec2e32cdcc2c9240198f0732300d5a5a7f959c8
Anselm Strauss <amsibamsi@gmail.com>
2016-11-12 10:58:28 +0100

    added parallel merge sort

--- mergesort/main.go
+++ mergesort/main.go
@@ -1,5 +1,7 @@
 package mergesort
 
+import "sync"
+
 func MergeSort(a []int) {
 	length := len(a)
 	buf := make([]int, length)
@@ -31,6 +33,48 @@ func MergeSort(a []int) {
 				right++
 			}
 		}
+		src, dst = dst, src
+	}
+	copy(a, src)
+}
+
+func ParMergeSort(a []int) {
+	length := len(a)
+	buf := make([]int, length)
+	src := a
+	dst := buf
+	wg := sync.WaitGroup{}
+	for size := 1; size < length; size *= 2 {
+		for part := 0; part < length; part += 2 * size {
+			wg.Add(1)
+			go func(part int) {
+				dstInd := part
+				left := part
+				right := part + size
+				for left < part+size && left < length && right < part+2*size && right < length {
+					if src[left] <= src[right] {
+						dst[dstInd] = src[left]
+						left++
+					} else {
+						dst[dstInd] = src[right]
+						right++
+					}
+					dstInd++
+				}
+				for left < part+size && left < length {
+					dst[dstInd] = src[left]
+					dstInd++
+					left++
+				}
+				for right < part+2*size && right < length {
+					dst[dstInd] = src[right]
+					dstInd++
+					right++
+				}
+				wg.Done()
+			}(part)
+		}
+		wg.Wait()
 		src, dst = dst, src
 	}
 	copy(a, src)
--- mergesort/main_test.go
+++ mergesort/main_test.go
@@ -2,11 +2,15 @@ package mergesort
 
 import (
 	"fmt"
+	"math/rand"
+	"sort"
 	"testing"
+	"time"
 )
 
 var (
-	Tests = []struct {
+	MaxInt = int(^uint(0) >> 1)
+	Tests  = []struct {
 		original, sorted []int
 	}{
 		{[]int{3, 8, 7, 5, 6, 1, 9, 2, 4}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}},
@@ -15,6 +19,14 @@ var (
 		{[]int{1, 1, 1, 2, 1e10, 1, 1, 1}, []int{1, 1, 1, 1, 1, 1, 2, 1e10}},
 	}
 )
+
+func randArray(length, max int) []int {
+	r := make([]int, length)
+	for i := 0; i < len(r); i++ {
+		r[i] = rand.Intn(max)
+	}
+	return r
+}
 
 func TestMergeSort(t *testing.T) {
 	for _, test := range Tests {
@@ -28,5 +40,80 @@ func TestMergeSort(t *testing.T) {
 				}
 			}
 		})
+	}
+}
+
+func TestMergeSortRandom(t *testing.T) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	for i := 0; i < 1e3; i++ {
+		t.Run(fmt.Sprintf("random %v", i), func(t *testing.T) {
+			a := randArray(1e3, MaxInt)
+			b := make([]int, len(a))
+			copy(b, a)
+			sort.Ints(a)
+			MergeSort(b)
+			for j := range a {
+				if b[j] != a[j] {
+					t.Fatalf("got '%v', expected '%v'", b[j], a[j])
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkMergeSort(b *testing.B) {
+	rand.Seed(0)
+	for i := 0; i < b.N; i++ {
+		a := randArray(1e4, MaxInt)
+		MergeSort(a)
+	}
+}
+
+func TestParMergeSort(t *testing.T) {
+	for _, test := range Tests {
+		t.Run(fmt.Sprintf("%v", test.original), func(t *testing.T) {
+			l := make([]int, len(test.original))
+			copy(l, test.original)
+			ParMergeSort(l)
+			for i := range test.sorted {
+				if l[i] != test.sorted[i] {
+					t.Fatalf("got '%v', expected '%v'", l, test.sorted)
+				}
+			}
+		})
+	}
+}
+
+func TestParMergeSortRandom(t *testing.T) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	for i := 0; i < 1e3; i++ {
+		t.Run(fmt.Sprintf("random %v", i), func(t *testing.T) {
+			a := randArray(1e3, MaxInt)
+			b := make([]int, len(a))
+			copy(b, a)
+			sort.Ints(a)
+			ParMergeSort(b)
+			for j := range a {
+				if b[j] != a[j] {
+					t.Fatalf("got '%v', expected '%v'", b[j], a[j])
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkParMergeSort(b *testing.B) {
+	rand.Seed(0)
+	for i := 0; i < b.N; i++ {
+		a := randArray(1e4, MaxInt)
+		ParMergeSort(a)
+	}
+}
+
+func BenchmarkSort(b *testing.B) {
+	rand.Seed(0)
+	for i := 0; i < b.N; i++ {
+		a := randArray(1e4, MaxInt)
+		sort.Ints(a)
 	}
 }
